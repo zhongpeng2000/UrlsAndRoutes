ff820d1d9d8ff96d956d7d21e9a12948e80cb7f8
zhongpeng <zhongpeng2000@gmail.com>
2016-08-25 23:57:13 -0700

    ROUTERS

--- UrlsAndRoutes/Controllers/HomeController.cs
+++ UrlsAndRoutes/Controllers/HomeController.cs
@@ -15,5 +15,21 @@ namespace UrlsAndRoutes.Controllers
             ViewBag.Action = "Index";
             return View("ActionName");
         }
+
+        public ActionResult CustomVariable(string id = "DefaultId")
+        {
+            ViewBag.Controller = "Admin";
+            ViewBag.Action = "CustomVariable";
+            ViewBag.CustomVariable = id ;
+            return View("ActionName");
+        }
+
+        public ViewResult MyActionMethod()
+        {
+            string myActionUrl = Url.Action("Index", new { id = "MyID" });
+            string myRouteUrl = Url.RouteUrl(new { controller = "Home", action = "Index" });
+
+            return View();
+        }
     }
 }
